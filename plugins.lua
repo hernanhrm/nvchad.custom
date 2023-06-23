5e843d46d3a7426082e2b997c4b894e9b7b7d8cb
Hernan Reyes <hernan_rm@outlook.es>
2023-06-23 12:43:00 -0600

    Feat: adds zen mode plugin and change theme

--- plugins.lua
+++ plugins.lua
@@ -1,83 +1,98 @@
-local overrides = require "custom.configs.overrides"
+local overrides = require("custom.configs.overrides")
 
 local plugins = {
-  {
-    "williamboman/mason.nvim",
-    opts = overrides.mason,
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = overrides.treesitter,
-  },
+	{
+		"williamboman/mason.nvim",
+		opts = overrides.mason,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = overrides.treesitter,
+	},
 
-  {
-    "nvim-tree/nvim-tree.lua",
-    opts = overrides.nvimtree,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-    end,
-  },
-  {
-    "jose-elias-alvarez/null-ls.nvim",
-    ft = { "lua", "go", "python", "html", "css", "json", "javascript", "sql", "typescript" },
-    opts = function()
-      return require "custom.configs.null-ls"
-    end,
-  },
-  {
-    "mfussenegger/nvim-dap",
-  },
-  {
-    "leoluz/nvim-dap-go",
-    ft = "go",
-    dependencies = "mfussenegger/nvim-dap",
-    config = function(_, opts)
-      require("dap-go").setup(opts)
-      -- require("core.utils").load_mappings("dap_go")
-    end,
-  },
-  {
-    "olexsmir/gopher.nvim",
-    ft = "go",
-    config = function(_, opts)
-      require("gopher").setup(opts)
-    end,
-    build = function()
-      vim.cmd [[silent! GoInstallDeps]]
-    end,
-  },
-  {
-    "ThePrimeagen/harpoon",
-    lazy = false,
-    dependencies = "nvim-lua/plenary.nvim",
-    priority = 0,
-  },
-  {
-    "folke/trouble.nvim",
-    dependencies = "nvim-tree/nvim-web-devicons",
-  },
-  {
-    "github/copilot.vim",
-    lazy = false,
-  },
-  {
-    "folke/zen-mode.nvim",
-    config = function()
-      require("zen-mode").setup()
-    end,
-  },
-  {
-    "NvChad/nvterm",
-    enabled = false,
-  },
-  {
-    "wakatime/vim-wakatime",
-    lazy = false,
-  },
+	{
+		"nvim-tree/nvim-tree.lua",
+		opts = overrides.nvimtree,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.configs.lspconfig")
+		end,
+	},
+	{
+		"jose-elias-alvarez/null-ls.nvim",
+		ft = { "lua", "go", "python", "html", "css", "json", "javascript", "sql", "typescript" },
+		opts = function()
+			return require("custom.configs.null-ls")
+		end,
+	},
+	{
+		"mfussenegger/nvim-dap",
+	},
+	{
+		"leoluz/nvim-dap-go",
+		ft = "go",
+		dependencies = "mfussenegger/nvim-dap",
+		config = function(_, opts)
+			require("dap-go").setup(opts)
+			-- require("core.utils").load_mappings("dap_go")
+		end,
+	},
+	{
+		"olexsmir/gopher.nvim",
+		ft = "go",
+		config = function(_, opts)
+			require("gopher").setup(opts)
+		end,
+		build = function()
+			vim.cmd([[silent! GoInstallDeps]])
+		end,
+	},
+	{
+		"ThePrimeagen/harpoon",
+		lazy = false,
+		dependencies = "nvim-lua/plenary.nvim",
+		priority = 0,
+	},
+	{
+		"folke/trouble.nvim",
+		dependencies = "nvim-tree/nvim-web-devicons",
+	},
+	{
+		"github/copilot.vim",
+		lazy = false,
+	},
+	{
+		"NvChad/nvterm",
+		enabled = false,
+	},
+	{
+		"wakatime/vim-wakatime",
+		lazy = false,
+	},
+	{
+		"folke/zen-mode.nvim",
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+			window = {
+				options = {
+					number = true,
+					relativenumber = true,
+				},
+			},
+			plugins = {
+				alacritty = {
+					enabled = true,
+					font = "12",
+				},
+			},
+		},
+		lazy = false,
+	},
 }
 
 return plugins
